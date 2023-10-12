9dca343dd3d1504f3f9f99c9d966fb8aeb46a84c
Catalin Stan <catalin.stan@endava.com>
2023-10-12 11:32:29 +0300

    Debug changes

--- scream/ScreamWrapper.cpp
+++ scream/ScreamWrapper.cpp
@@ -4,7 +4,7 @@
 #include <string.h> 
 #include <cassert>
 #include <sys/time.h>
-#include <pthread.h>
+#include <mutex>
 #include <iostream>
 #include "netinet/in.h"
 
@@ -14,40 +14,41 @@
 
 using namespace std;
 
-static pthread_mutex_t lock_scream;
+namespace
+{
+
+std::mutex lock_scream;
 double t0 = 0;
 
-uint32_t getTimeInNtp(){
+uint32_t getTimeInNtp()
+{
   struct timeval tp;
   gettimeofday(&tp, NULL);
-  double time = tp.tv_sec + tp.tv_usec*1e-6-t0;
+  double time = tp.tv_sec + tp.tv_usec*1e-6 - t0;
   uint64_t ntp64 = uint64_t(time*65536.0);
   uint32_t ntp = 0xFFFFFFFF & ntp64;
   return ntp;
 }
 
-void parseRtp(char* buf, uint16_t* seqNr, uint32_t* timeStamp) {
-	uint16_t rawSeq;
-	uint32_t rawTs;
-	memcpy(&rawSeq, buf + 2, 2);
-	memcpy(&rawTs, buf + 4, 4);
-	*seqNr = ntohs(rawSeq);
-	*timeStamp = ntohl(rawTs);
-}
-
-class ScreamRxProxy{
-	
+class ScreamRxProxy
+{
 	public:
-		ScreamRxProxy(){
+		ScreamRxProxy()
+        {
 			   struct timeval tp;
 			   gettimeofday(&tp, NULL);
 			   t0 = tp.tv_sec + tp.tv_usec*1e-6;
-		};
+
+               screamRx = new ScreamRx(SSRC, ACK_DIFF, REPORTED_RTP_PACKETS);
+		}
 		
-		void receive(char* buf, int recvlen, unsigned char received_ecn, bool isMark){
+		void receive(uint16_t seqNr, uint32_t ts, char* buf, int size, unsigned char received_ecn, bool isMark)
+        {
+            std::lock_guard<std::mutex> lock { lock_scream };
+
 			uint32_t time_ntp = getTimeInNtp();
-			pthread_mutex_lock(&lock_scream);
-			if (time_ntp - last_received_time_ntp > 2 * 65536) { // 2 sec
+			if (time_ntp - last_received_time_ntp > 2 * 65536)
+            { // 2 sec
 				receivedRtp = 0;
 				delete screamRx;
 				screamRx = new ScreamRx(SSRC, ACK_DIFF, REPORTED_RTP_PACKETS);
@@ -57,50 +58,50 @@ class ScreamRxProxy{
 			last_received_time_ntp = time_ntp;
 			receivedRtp++;
 			
-			uint16_t seqNr;
-			uint32_t ts;
-			parseRtp(buf, &seqNr, &ts);
+			ts = time_ntp; // test
 
 			if ((seqNr - lastSn) > 1) {
 				fprintf(stderr, "Packet(s) lost or reordered : %5d was received, previous rcvd is %5d \n", seqNr, lastSn);
 			}
 			lastSn = seqNr;
 			
-			screamRx->receive(getTimeInNtp(), 0, SSRC, recvlen, seqNr, received_ecn, isMark);
-			pthread_mutex_unlock(&lock_scream);
+			screamRx->receive(getTimeInNtp(), 0, SSRC, size, seqNr, received_ecn, isMark);
+	  }
 	  
-	  };
-	  
-	  bool getFeedback(unsigned char *buf, int *rtcpSize){
-		  
-		uint32_t time_ntp = getTimeInNtp();
-		bool isFeedback = false;
-	    pthread_mutex_lock(&lock_scream);
-		uint32_t rtcpFbInterval_ntp = screamRx->getRtcpFbInterval();
-		
-		if (screamRx->isFeedback(time_ntp) && (screamRx->checkIfFlushAck() ||
-			(time_ntp - screamRx->getLastFeedbackT() > rtcpFbInterval_ntp))) {
-				isFeedback = screamRx->createStandardizedFeedback(getTimeInNtp(), true, buf, *rtcpSize);
-			}
-		pthread_mutex_unlock(&lock_scream);
-		
-		return isFeedback;
-	  };
+	  bool getFeedback(unsigned char *buf, int *rtcpSize)
+      {
+        std::lock_guard<std::mutex> lock { lock_scream };
+
+        uint32_t time_ntp = getTimeInNtp();
+        bool isFeedback = false;
+        uint32_t rtcpFbInterval_ntp = screamRx->getRtcpFbInterval();
+
+        if (screamRx->isFeedback(time_ntp) && (screamRx->checkIfFlushAck() ||
+            (time_ntp - screamRx->getLastFeedbackT() > rtcpFbInterval_ntp)))
+        {
+            isFeedback = screamRx->createStandardizedFeedback(getTimeInNtp(), true, buf, *rtcpSize);
+        }
+
+        return isFeedback;
+	  }
 		
 	private:
-		ScreamRx* screamRx;
+		ScreamRx* screamRx = nullptr;
 	    uint32_t last_received_time_ntp = 0;
 	    uint32_t receivedRtp = 0;
 		uint16_t lastSn = 0;
-	
 };
 
-static ScreamRxProxy sx;
+ScreamRxProxy sx;
+} // namspace
 
-void screamReceive(char* buffer, int recvLength, unsigned char ecn, bool isMark){
-	  sx.receive(buffer, recvLength, ecn, isMark);
+void screamReceive(uint16_t seqNr, uint32_t ts, char* buffer, int size, unsigned char ecn, bool isMark)
+{
+	  sx.receive(seqNr, ts, buffer, size, ecn, isMark);
 };
 
-bool screamGetFeedback(unsigned char *buf, int *size){
+bool screamGetFeedback(unsigned char *buf, int *size)
+{
 	return sx.getFeedback(buf, size);
 };
+
--- scream/ScreamWrapper.h
+++ scream/ScreamWrapper.h
@@ -1,9 +1,11 @@
 #ifdef __cplusplus
+#pragma once
+
 extern "C"
 {
 #endif
 
-void screamReceive(char*, int, unsigned char, bool);
+void screamReceive(unsigned short seqNr, unsigned int ts, char *buf, int size, unsigned char ecn, bool isMark);
 bool screamGetFeedback(unsigned char *buf, int *size);
 
 #ifdef __cplusplus
